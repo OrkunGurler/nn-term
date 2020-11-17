31590660e571366f1bea2885b1585673cbb3fd83
Orkun Gürler <orkungurler@gmail.com>
2020-11-17 11:41:14 +0300

    add matrix operations in js

---
+++ MultiLayerPerceptron/js/matrix.js
@@ -0,0 +1,69 @@
+class Matrix {
+    constructor(rows, cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.data = [];
+
+        for (let i = 0; i < this.rows; i++) {
+            this.data[i] = [];
+            for (let j = 0; j < this.cols; j++) {
+                this.data[i][j] = 0;
+            }
+        }
+    }
+
+    randomize(a, b) {
+        if (a > b) {
+            console.log("[ERR: randomize] First value must be lower than the second one!");
+            return undefined;
+        }
+        if (b <= 0) {
+            [a, b] = [b, a];
+        }
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                this.data[i][j] = (Math.random() * (b - a)) + a;
+            }
+        }
+    }
+
+    map(func) {
+        let val;
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                val = this.data[i][j];
+                this.data[i][j] = func(val);
+            }
+        }
+    }
+
+    transpose() {
+        let m_t = new Matrix(this.cols, this.rows);
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                m_t.data[j][i] = this.data[i][j];
+            }
+        }
+        return m_t;
+    }
+
+    static dotproduct(m1, m2) {
+        if (m1.cols !== m2.rows) {
+            console.log("[ERR: dotproduct] First matrix's column and second matrix's row sizes are not equal!");
+            return undefined;
+        }
+        let m = new Matrix(m1.rows, m2.cols);
+        for (let i = 0; i < m.rows; i++) {
+            for (let j = 0; j < m.cols; j++) {
+                for (let k = 0; k < m1.cols; k++) {
+                    m.data[i][j] += m1.data[i][k] * m2.data[k][j];
+                }
+            }
+        }
+        return m;
+    }
+
+    print() {
+        console.table(this.data);
+    }
+}
