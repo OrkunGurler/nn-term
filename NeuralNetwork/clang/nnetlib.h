fb65ea9af0d8da151fc5e9425607dc66fc8351a8
Orkun Gürler <orkungurler@gmail.com>
2020-12-08 22:04:41 +0300

    add nnetlib

--- NeuralNetwork/clang/nnetlib.h
+++ NeuralNetwork/clang/nnetlib.h
@@ -1,30 +1,6 @@
-#include "activlib.h"
-#include "trainlib.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
 
-typedef struct neuron
-{
-    int input;
-    int hidden;
-    int output;
-} neuron;
-neuron setneurons(int input, int hidden, int output);
-
-typedef struct weight
-{
-    mx i2h;
-    mx i2hb;
-    mx *h2h;
-    mx *h2hb;
-    mx h2o;
-    mx h2ob;
-} weight;
-weight setweights(neuron neurons, int hcount, int flag);
-
-typedef struct neuralnetwork
-{
-    neuron neurons;
-    weight weights;
-    activ activation;
-    float learningrate;
-} nnet;
-nnet createnetwork(neuron neurons, weight weights, activ activation, float learningrate);
+#define INF 0xFFFFFFFF
