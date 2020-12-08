b298d91306e03f0ec94c241d4d90b7a0bcfc4637
Orkun Gürler <orkungurler@gmail.com>
2020-12-08 22:03:48 +0300

    fix var names and libs

--- NeuralNetwork/clang/activlib.c
+++ NeuralNetwork/clang/activlib.c
@@ -1,54 +1,45 @@
-#include <math.h>
 #include "activlib.h"
 
-activ setactiv(float (*func)(float), float (*func_d)(float))
-{
-    activ act;
-    act.func = func;
-    act.func_d = func_d;
-    return act;
-}
-
 // Identity
-float Identity(float x) { return x; }
-float Identity_d(float x) { return 1; }
+float identity(float x) { return x; }
+float identity_d(float x) { return 1; }
 
 // Binary Step
-float BinaryStep(float x) { return (x < 0 ? 0 : 1); }
-float BinaryStep_d(float x) { return (x != 0 ? 0 : INF); }
+float binary_step(float x) { return (x < 0 ? 0 : 1); }
+float binary_step_d(float x) { return (x != 0 ? 0 : INF); }
 
 // Sigmoid
-float Sigmoid(float x) { return (1 / (1 + exp(-x))); }
-float Sigmoid_d(float x) { return (Sigmoid(x) * (1 - Sigmoid(x))); }
+float sigmoid(float x) { return (1 / (1 + exp(-x))); }
+float sigmoid_d(float x) { return (sigmoid(x) * (1 - sigmoid(x))); }
 
 // TanH
-float TanH(float x) { return ((exp(x) - exp(-x)) / (exp(x) + exp(-x))); }
-float TanH_d(float x) { return (1 - pow(TanH(x), 2)); }
+float tanh(float x) { return ((exp(x) - exp(-x)) / (exp(x) + exp(-x))); }
+float tanh_d(float x) { return (1 - pow(tanh(x), 2)); }
 
 // Rectified Linear Unit
-float ReLU(float x) { return (x > 0 ? x : 0); }
-float ReLU_d(float x) { return (x > 0 ? 1 : 0); }
+float relu(float x) { return (x > 0 ? x : 0); }
+float relu_d(float x) { return (x > 0 ? 1 : 0); }
 
 // Gaussian Error Linear Unit
-// float GELU(float x) {}
-// float GELU_d(float x) {}
+// float gelu(float x) {}
+// float gelu_d(float x) {}
 
 // SoftPlus
-float SoftPlus(float x) { return (log(1 + exp(x))); }
-float SoftPlus_d(float x) { return (1 / (1 + exp(-x))); }
+float softplus(float x) { return (log(1 + exp(x))); }
+float softplus_d(float x) { return (1 / (1 + exp(-x))); }
 
 // Exponential Linear Unit
-float ELU(float x, float alpha) { return (x > 0 ? x : (alpha * (exp(x) - 1))); }
-float ELU_d(float x, float alpha) { return (x > 0 ? 1 : (ELU(x, alpha) + alpha)); }
+float elu(float x, float alpha) { return (x > 0 ? x : (alpha * (exp(x) - 1))); }
+float elu_d(float x, float alpha) { return (x > 0 ? 1 : (elu(x, alpha) + alpha)); }
 
 // Scaled Exponential Linear Unit
-float SELU(float x)
+float selu(float x)
 {
     const float lambda = 1.0507;
     const float alpha = 1.67326;
     return (lambda * (x < 0 ? (alpha * (exp(x) - 1)) : x));
 }
-float SELU_d(float x)
+float selu_d(float x)
 {
     const float lambda = 1.0507;
     const float alpha = 1.67326;
@@ -56,29 +47,29 @@ float SELU_d(float x)
 }
 
 // Leaky Rectified Linear Unit
-float Leaky_ReLU(float x, float alpha) { return (x < 0 ? (0.01 * x) : x); }
-float Leaky_ReLU_d(float x, float alpha) { return (x < 0 ? 0.01 : x); }
+float leaky_relu(float x, float alpha) { return (x < 0 ? (0.01 * x) : x); }
+float leaky_relu_d(float x, float alpha) { return (x < 0 ? 0.01 : x); }
 
 // Parameteric Rectified Linear Unit
-float PReLU(float x, float alpha) { return (x < 0 ? (alpha * x) : x); }
-float PReLU_d(float x, float alpha) { return (x < 0 ? alpha : 1); }
+float prelu(float x, float alpha) { return (x < 0 ? (alpha * x) : x); }
+float prelu_d(float x, float alpha) { return (x < 0 ? alpha : 1); }
 
 // ArcTan
-float ArcTan(float x) { return (atan(x)); }
-float ArcTan_d(float x) { return (1 / (pow(x, 2) + 1)); }
+float arctan(float x) { return (atan(x)); }
+float arctan_d(float x) { return (1 / (pow(x, 2) + 1)); }
 
 // ElliotSig
-float ElliotSig(float x)
+float elliot_sig(float x)
 {
     return (x / (1 + (x < 0 ? -x : x)));
 }
-float ElliotSig_d(float x)
+float elliot_sig_d(float x)
 {
     return (1 / pow(1 + (x < 0 ? -x : x), 2));
 }
 
 // Square Nonlinearity
-float SQNL(float x)
+float sqnl(float x)
 {
     if (x > 2)
     {
@@ -97,32 +88,32 @@ float SQNL(float x)
         return -1;
     }
 }
-float SQNL_d(float x) { return (x < 0 ? (1 + (x / 2)) : (1 - (x / 2))); }
+float sqnl_d(float x) { return (x < 0 ? (1 + (x / 2)) : (1 - (x / 2))); }
 
 // S-Shaped Rectified Linear Activation Unit
 
 // Bent identity
-float BentIdentity(float x) { return (((sqrt(pow(x, 2) + 1) - 1) / 2) + x); }
-float BentIdentity_d(float x) { return ((x / (2 * sqrt(pow(x, 2) + 1))) + 1); }
+float bent_identity(float x) { return (((sqrt(pow(x, 2) + 1) - 1) / 2) + x); }
+float bent_identity_d(float x) { return ((x / (2 * sqrt(pow(x, 2) + 1))) + 1); }
 
 // Sigmoid Linear Unit
-float SiLU(float x) { return (x / (1 + exp(-x))); }
-float SiLU_d(float x) { return ((1 + exp(-x) + (x * exp(-x))) / pow(1 + exp(-x), 2)); }
+float silu(float x) { return (x / (1 + exp(-x))); }
+float silu_d(float x) { return ((1 + exp(-x) + (x * exp(-x))) / pow(1 + exp(-x), 2)); }
 
 // Sinusoid
-float Sinusoid(float x) { return (sin(x)); }
-float Sinusoid_d(float x) { return (cos(x)); }
+float sinusoid(float x) { return (sin(x)); }
+float sinusoid_d(float x) { return (cos(x)); }
 
 // Sinc
-float Sinc(float x) { return (x == 0 ? 1 : (sin(x) / x)); }
-float Sinc_d(float x) { return (x == 0 ? 0 : ((cos(x) / x) - (sin(x) / pow(x, 2)))); }
+float sinc(float x) { return (x == 0 ? 1 : (sin(x) / x)); }
+float sinc_d(float x) { return (x == 0 ? 0 : ((cos(x) / x) - (sin(x) / pow(x, 2)))); }
 
 // Gaussian
-float Gaussian(float x) { return (exp(-pow(x, 2))); }
-float Gaussian_d(float x) { return (-2 * x * exp(-pow(x, 2))); }
+float gaussian(float x) { return (exp(-pow(x, 2))); }
+float gaussian_d(float x) { return (-2 * x * exp(-pow(x, 2))); }
 
 // SQ-RBF
-float SQRBF(float x)
+float sqrbf(float x)
 {
     float y = (x < 0 ? -x : x);
     if (y <= 1)
@@ -138,7 +129,7 @@ float SQRBF(float x)
         return 0;
     }
 }
-float SQRBF_d(float x)
+float sqrbf_d(float x)
 {
     float y = (x < 0 ? -x : x);
     if (y <= 1)
@@ -147,7 +138,8 @@ float SQRBF_d(float x)
     }
     else if ((y > 1) && (y < 2))
     {
-        return (x - (2 * (x > 0 ? 1 : x == 0 ? 0 : -1)));
+        return (x - (2 * (x > 0 ? 1 : x == 0 ? 0
+                                             : -1)));
     }
     else
     {
@@ -158,5 +150,5 @@ float SQRBF_d(float x)
 // Softmax
 
 // Maxout
-// Matrix Maxout(Matrix x) {}
-// Matrix Maxout_d(Matrix x) {}
+// Matrix maxout(Matrix x) {}
+// Matrix maxout_d(Matrix x) {}
