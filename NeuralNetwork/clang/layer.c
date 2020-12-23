8034923768b7877b010f50cd221b7603d7e3546d
Orkun Gürler <orkungurler@gmail.com>
2020-12-23 23:25:33 +0300

    add prev param at layer

--- NeuralNetwork/clang/layer.c
+++ NeuralNetwork/clang/layer.c
@@ -3,16 +3,18 @@
 layer create_layers(int input_node_size, int hidden_layer_node_size, int hidden_layer_size, int output_node_size)
 {
     layer input_layer = (layer)malloc(sizeof(LAYER_T));
-
     input_layer->nodes = create_matrix(input_node_size, 1);
     input_layer->weights = create_matrix(hidden_layer_node_size, input_node_size);
     randomize(input_layer->weights, 0, 1);
     input_layer->bias = (float)rand() / (float)(RAND_MAX);
+    input_layer->prev = NULL;
 
     layer next_hidden_layer = (layer)malloc(sizeof(LAYER_T));
-
     input_layer->next = next_hidden_layer;
+    next_hidden_layer->prev = input_layer;
+
     int i;
+    layer prev;
     for (i = 0; i < hidden_layer_size; i++)
     {
         next_hidden_layer->nodes = create_matrix(hidden_layer_node_size, 1);
@@ -27,11 +29,13 @@ layer create_layers(int input_node_size, int hidden_layer_node_size, int hidden_
         randomize(next_hidden_layer->weights, 0, 1);
         next_hidden_layer->bias = (float)rand() / (float)(RAND_MAX);
         next_hidden_layer->next = (layer)malloc(sizeof(LAYER_T));
+        prev = next_hidden_layer;
         next_hidden_layer = next_hidden_layer->next;
+        next_hidden_layer->prev = prev;
     }
 
     layer output_layer = next_hidden_layer;
-    
+
     output_layer->nodes = create_matrix(output_node_size, 1);
     output_layer->next = NULL;
 
--- NeuralNetwork/clang/layer.h
+++ NeuralNetwork/clang/layer.h
@@ -6,6 +6,7 @@ typedef struct LAYER_S
     mx nodes;
     mx weights;
     float bias;
+    layer prev;
     layer next;
 } LAYER_T[1];
 layer create_layers(int input_size, int hidden_layer_node_size, int hidden_layer_size, int output_node_size);
