98d338a5f6bd4ca516b3bdb5eac966fe6f6af8f3
Orkun Gürler <orkungurler@gmail.com>
2020-12-23 23:26:27 +0300

    edit main

--- NeuralNetwork/clang/main.c
+++ NeuralNetwork/clang/main.c
@@ -3,19 +3,23 @@
 int main(int argc, char const *argv[])
 {
     // i.e. 8 * 8 - 2D Canvas
-    int size = 8 * 8;
-    mx input_data = create_matrix(size, 1);
+    mx input_data = create_matrix(8, 8);
+    int input_size = input_data.rows * input_data.cols;
     randomize(input_data, 0, 1);
-    int hidden_layer_node_size = pow(2, size);
-    int hidden_layer_size = size;
-    int output_node_size = sqrt(size);
+    int hidden_layer_node_size = pow(2, input_size);
+    int hidden_layer_size = input_size;
+    int output_node_size = sqrt(input_size);
 
-    nnet nnet = create_network(input_data, hidden_layer_node_size, hidden_layer_size, output_node_size);
+    nnet nnet;
+    nnet.layers = create_layers(input_size, hidden_layer_node_size, hidden_layer_size, output_node_size);
 
-    mx input_for_prediction = create_matrix(pow(size, 2), 1);
+    mx input_for_train = create_matrix(pow(input_size, 2), 1);
+    backpropagation(nnet, input_for_train);
+
+    mx input_for_prediction = create_matrix(pow(input_size, 2), 1);
     randomize(input_for_prediction, 0, 1);
 
-    predict(nnet, input_for_prediction); // TODO: fix predicted values (all values are same)
+    feed_forward(nnet, input_for_prediction); // TODO: fix predicted values (all values are same)
 
     return 0;
 }
